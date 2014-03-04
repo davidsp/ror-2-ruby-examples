b89d21c20b4d3f955bf9dc3ee9c9785df17dbe14
davidsp <davidsp@gmail.com>
2014-03-04 15:28:39 +0100

    exercises 5-10

---
+++ learn-the-hard-way/ex10.rb
@@ -0,0 +1,16 @@
+
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat =  "I'm \\ a \\ cat"
+
+fat_cat = <<MY_ANYTHING
+I'll do a list:
+\t * Cat food
+\t * fishes
+\t * sand
+MY_ANYTHING
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
---
+++ learn-the-hard-way/ex9.rb
@@ -0,0 +1,14 @@
+
+#some particular stuff
+
+days = "mon tue wed thu frid sun"
+months =  "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAgo"
+
+puts "here are the days: ", days
+puts "here the modnts: ", months
+
+puts <<PARAGRAPH
+something in sentence.
+line break
+yet another one
+PARAGRAPH
